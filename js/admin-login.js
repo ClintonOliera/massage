c5c5f29b37b975990dd266aee5af54f7d8e3f080
ClintonOliera <clintonoliera391@gmail.com>
2025-11-14 03:32:35 +0300

    fixed admin login

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://lavylotus.onrender.com/api';
 
 document.getElementById('adminLoginForm').addEventListener('submit', async (e) => {
   e.preventDefault();
